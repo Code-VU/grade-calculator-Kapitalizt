a3ea88f4c0dc3169c906a10f19ca6964ae2112c2
Rob Kirchoff <Rob.Kirchoff@vu.com>
2022-04-13 10:55:30 -0500

    try1000

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -8,22 +8,20 @@ def calculateGrade():
     except: 
         print("Calculating Grade")
         print('Bad score')
-        quit()
-    
-    if score < 0 or score > 1:
-        print('Bad score')
-        quit()
-                
-    if score >= 0.9: 
-        print('A')
-    elif score >= 0.8: 
-        print('B')
-    elif score >= 0.7: 
-        print('C')
-    elif score >= 0.6: 
-        print('D')
-    elif score < 0.6: 
-        print('F')
+        quit()         
+    if score > 0.0 and score < 1.0:
+        if score >= 0.9:
+            print('A')
+        elif score >= 0.8:
+            print('B')
+        elif score >= 0.7:
+            print('C')
+        elif score >= 0.6:
+            print('D')
+        else :
+            print('F')
+    else:
+        print('Bad Score')  
     # end assignment
 
 ## if you want to test locally before you try to sync
